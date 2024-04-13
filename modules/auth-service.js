8d61a4e12743d0a81e205f65eb2c4659333a5547
Ulas Ondev <ulascondev@gmail.com>
2024-04-12 22:37:39 -0400

    a6

---
+++ modules/auth-service.js
@@ -0,0 +1,109 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { resolve } = require('path');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+        userName: {
+            type: String,
+            unique: true 
+        },
+        password: String,
+        email: String,
+        loginHistory: [{
+            dateTime: { type: Date, default: Date.now },
+            userAgent:{type: String}
+        }]
+    }
+)
+
+let User; //= mongoose.model('Users', userSchema);
+
+function initialize() {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection(process.env.MONGODB);
+
+        db.on('error', (err) => {
+            reject(err); 
+        });
+        db.once('open', () => {
+            User = db.model("users", userSchema);
+            console.log('successfully connected to mongodb');
+            resolve();
+        });
+    });
+}
+
+function registerUser(userData) {
+    return new Promise((resolve, reject) => {
+        if (userData.password != userData.password2) {
+            reject('Passwords do not match');
+        } else {
+            bcrypt.hash(userData.password, 10)
+                .then(hash => {
+                    userData.password = hash;
+
+                    let newUser = new User(userData);
+                    
+                    newUser.save()
+                        .then(() => {
+                            console.log(`This is Register function: user successfully created in the database`);
+                            resolve(); 
+                        })
+                        .catch((err) => {
+                            if (err.code == 11000) {
+                                reject("Username already taken");
+                            } else {
+                                reject(`There was an error creating the user: ${err}`);
+                            }
+                        });
+                })
+                .catch(err => {
+                    console.log(`Hashing interrupted: ${err}`);
+                    reject(`There was an error encrypting the password`);
+                });
+        }
+    });
+}
+
+function checkUser(userData) {
+    return new Promise((resolve, reject) => {
+      User.find({ userName: userData.userName })
+        .exec()
+        .then((users) => {
+          if (users.length == 0) {
+            reject("Unable to find user: " + userData.userName);
+          } else {
+            bcrypt.compare(userData.password, users[0].password).then((res) => {
+              if (res == true) {
+                if (users[0].loginHistory.length == 8) {
+                  users[0].loginHistory.pop(); 
+                }
+                users[0].loginHistory.unshift({
+                  dateTime: new Date().toString(),
+                  userAgent: userData.userAgent,
+                }); 
+                User.updateOne(
+                  { userName: users[0].userName },
+                  { $set: { loginHistory: users[0].loginHistory } }
+                )
+                  .exec()
+                  .then(() => {
+                    resolve(users[0]);
+                  })
+                  .catch((err) => {
+                    reject("There was an error verifying the user: " + err);
+                  });
+              } else {
+                reject("Incorrect Password for user: " + userData.userName);
+              }
+            });
+          }
+        })
+        .catch((err) => {
+          reject("Unable to find user: " + userData.userName);
+        });
+    });
+  }
+module.exports = { initialize, registerUser, checkUser }; 
--- server.js
+++ server.js
@@ -1,29 +1,52 @@
 /********************************************************************************
-* WEB322 – Assignment 05
+* WEB322 – Assignment 06
 *
 * I declare that this assignment is my own work in accordance with Seneca's
 * Academic Integrity Policy:
 *
 * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 *
-* Name: Ulas Cagin Ondev Student ID: 123734220 Date: March 21, 2024
+* Name: Ulas Cagin Ondev Student ID: 123734220 Date: April 12, 2024
 *
 * Published URL: https://sore-pear-alligator-tux.cyclic.app/
-*
+* 
 ********************************************************************************/
 
+const authData = require('./modules/auth-service');
 const legoData = require("./modules/legoSets");
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require('path');
+
+const clientSessions = require('client-sessions')//
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public')); 
+app.use(clientSessions({
+    cookieName: 'session',
+    secret: 'o78efhubdFGETnfoT04hfsYEeh37GD73hosf',
+    duration: 2 * 60 * 1000,
+    activeDuration: 1000 * 60,
+  }
+))
 
-app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
 
-const HTTP_PORT = process.env.PORT || 8080;
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    res.status(401).redirect('/login');
+  } else {
+    next();
+  }
+}
 
 app.get('/', (req, res) => {
   res.render("home")
@@ -33,89 +56,132 @@ app.get('/about', (req, res) => {
   res.render("about");
 });
 
-app.get('/lego/addSet', async (req, res) => {
-  try {
-    const themes = await legoData.getAllThemes();
-    res.render('addSet', { themes });
-  } catch (err) {
-    console.error(err);
-    res.status(500).render('500', { message: `I'm sorry, but we have encountered the following error: ${err}` });
-  }
+app.get("/lego/addSet", ensureLogin,async (req, res) => {
+  let themes = await legoData.getAllThemes()
+  res.render("addSet", { themes: themes })
 });
 
-app.post('/lego/addSet', async (req, res) => {
+app.post("/lego/addSet", ensureLogin,async (req, res) => {
   try {
     await legoData.addSet(req.body);
-    res.redirect('/lego/sets');
+    res.redirect("/lego/sets");
   } catch (err) {
-    console.error(err);
-    res.status(500).render('500', { message: `I'm sorry, but we have encountered the following error: ${err}` });
+    res.render("500", { Message: `I'm sorry, but we have encountered the following error: ${err}` });
+  }
+
+});
+
+app.get("/lego/editSet/:num",ensureLogin,async (req, res) => {
+
+  try {
+    let set = await legoData.getSetByNum(req.params.num);
+    let themes = await legoData.getAllThemes();
+
+    res.render("editSet", { set, themes });
+  } catch (err) {
+    res.status(404).render("404", { message: err });
+  }
+
+});
+
+app.post("/lego/editSet",ensureLogin,async (req, res) => {
+
+  try {
+    await legoData.editSet(req.body.set_num, req.body);
+    res.redirect("/lego/sets");
+  } catch (err) {
+    res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
   }
 });
 
-app.get("/lego/sets", async (req,res)=>{
+app.get("/lego/deleteSet/:num", ensureLogin,async (req, res) => {
+  try {
+    await legoData.deleteSet(req.params.num);
+    res.redirect("/lego/sets");
+  } catch (err) {
+    res.status(500).render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
+  }
+})
+
+app.get("/lego/sets", async (req, res) => {
 
   let sets = [];
 
-  try{    
-    if(req.query.theme){
+  try {
+    if (req.query.theme) {
       sets = await legoData.getSetsByTheme(req.query.theme);
-    }else{
+    } else {
       sets = await legoData.getAllSets();
     }
 
-    res.render("sets", {sets})
-  }catch(err){
-    res.status(404).render("404", {message: err});
+    res.render("sets", { sets })
+  } catch (err) {
+    res.status(404).render("404", { message: err });
   }
-  
+
 });
 
-app.get("/lego/sets/:num", async (req,res)=>{
-  try{
+
+app.get("/lego/sets/:num", async (req, res) => {
+  try {
     let set = await legoData.getSetByNum(req.params.num);
-    res.render("set", {set})
-  }catch(err){
-    res.status(404).render("404", {message: err});
+    res.render("set", { set })
+  } catch (err) {
+    res.status(404).render("404", { message: err });
   }
 });
 
-app.get('/lego/editSet/:num', async (req, res) => {
-  try {
-    const [set, themes] = await Promise.all([
-      legoData.getSetByNum(req.params.num),
-      legoData.getAllThemes(),
-    ]);
-
-    res.render('editSet', { set, themes });
-  } catch (error) {
-    res.status(404).render('404', { message: error.message });
-  }
+app.get('/register', (req, res) => {
+  res.render('register',{errorMessage:"", successMessage:"",userName:"", email: ""});
 });
 
-app.post('/lego/editSet', async (req, res) => {
-  try {
-    const setData = {
-      name: req.body.name,
-      year: req.body.year,
-      num_parts: req.body.num_parts,
-      img_url: req.body.img_url,
-      theme_id: req.body.theme_id,
-      set_num: req.body.set_num,
-    };
+app.get('/login', (req, res) => {
+  res.render('login', {errorMessage:"", userName:""});
+});
 
-    await legoData.editSet(req.body.set_num, setData);
 
-    res.redirect('/lego/sets');
-  } catch (error) {
-    res.status(500).render('500', { message: `Error editing set: ${error.message}` });
-  }
+app.post('/register', (req, res) => {
+  authData.registerUser(req.body).then(() => {
+    res.render('register', { errorMessage: "", successMessage: "User Created", userName: "", email: "" })
+  }).catch((err) => {
+    res.render('register', { errorMessage: err, successMessage: "", userName: req.body.userName, email: req.body.email });
+  }); 
+});
+
+app.post('/login', (req, res) => {
+
+  req.body.userAgent = req.get('User-Agent');
+  authData.checkUser(req.body)
+    .then((user) => {
+      req.session.user = {
+        userName: user.userName,
+        email: user.email,
+        loginHistory: user.loginHistory,
+      }
+      res.redirect('/lego/sets');
+    }).catch((err) => {
+      res.render('login', { errorMessage: err, userName: req.body.userName });
+    });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.reset();
+  res.redirect('/');
+});
+
+app.get('/userHistory', ensureLogin, (req, res) => {
+  res.render('userHistory');
 });
 
 app.use((req, res, next) => {
-  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+  res.status(404).render("404", { message: "I'm sorry, we're unable to find what you're looking for" });
 });
 
-legoData.initialize().then(()=>{
+
+legoData.initialize()
+  .then(authData.initialize)
+  .then(() => {
   app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+}).catch(err => {
+  console.log(err)
 });
